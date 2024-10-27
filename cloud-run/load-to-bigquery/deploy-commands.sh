aa9041090dfcf9cb9ed1d4058b6fc90b9327cb0d
tuanhiep <tuanhiep.tran.us@gmail.com>
2024-10-27 16:22:06 -0400

    Google App Engine using memcache

--- cloud-run/load-to-bigquery/deploy-commands.sh
+++ cloud-run/load-to-bigquery/deploy-commands.sh
@@ -7,3 +7,23 @@ gcloud builds submit --region us-central1
 
   gcloud projects add-iam-policy-binding gcp-serverless-project-374110 \
   --member=serviceAccount:131640033627@cloudbuild.gserviceaccount.com --role=roles/run.admin
+
+
+# My cloudbuild serviceAccount: 98179902139@cloudbuild.gserviceaccount.com
+# My project number: 98179902139
+# gcloud projects describe ferrous-depth-436501-q2 --format="value(projectNumber)"
+# => got this project number: 98179902139
+
+
+
+gcloud projects add-iam-policy-binding ferrous-depth-436501-q2 \
+    --member=serviceAccount:98179902139@cloudbuild.gserviceaccount.com \
+    --role=roles/storage.admin
+
+gcloud projects add-iam-policy-binding ferrous-depth-436501-q2 \
+    --member=serviceAccount:98179902139@cloudbuild.gserviceaccount.com \
+    --role=roles/run.admin
+
+gcloud projects add-iam-policy-binding ferrous-depth-436501-q2 \
+    --member=serviceAccount:98179902139@cloudbuild.gserviceaccount.com \
+    --role=roles/iam.serviceAccountUser
